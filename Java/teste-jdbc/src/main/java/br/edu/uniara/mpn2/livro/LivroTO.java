4049e797a55949bb24f672e91e1b800a2d1262d9
Caio Josef <102381112+caiojosef@users.noreply.github.com>
2023-03-17 19:08:07 -0300

    Initial Commit

---
+++ Java/teste-jdbc/src/main/java/br/edu/uniara/mpn2/livro/LivroTO.java
@@ -0,0 +1,37 @@
+package br.edu.uniara.mpn2.livro;
+
+public class LivroTO {
+	
+	private Integer idLivro;
+	
+	private String titulo;
+	
+	private String autor;
+
+	public Integer getIdLivro() {
+		return idLivro;
+	}
+
+	public void setIdLivro(Integer idLivro) {
+		this.idLivro = idLivro;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	
+	
+	
+}
